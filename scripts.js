b9957d823043cab4d80c269d513d3838983d8167
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-10-25 16:04:30 +0200

    Prevent empty form submission: Input fields cannot be empty on either one or both

--- scripts.js
+++ scripts.js
@@ -5,5 +5,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  if (dividend === "" || divider === "") {
+    alert('Division not performed. Both values are required in inputs. Try again.')
+    return; //stop function execution
+  }
   result.innerText = Math.floor(dividend / divider);
 });
